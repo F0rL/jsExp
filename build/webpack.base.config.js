70fb3e329cdba9589088fc3febe29a463b9e7b87
F0rL <kumaloveslife@gmail.com>
2020-08-01 23:51:05 +0800

    添加es6+进webpack配置

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,37 +1,48 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                            'useBuiltIns': 'entry'
-                        }]
-                    ]
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'entry'
                 }
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CopyWebpackPlugin([{
-            from: 'static',
-            to: 'static'
-        }, ])
-    ],
-    stats: {
-        children: false
-    }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'static',
+        to: 'static'
+      },
+      {
+        from: 'es6+',
+        to: 'es6+'
+      }
+    ])
+  ],
+  stats: {
+    children: false
+  }
 }
